5c543c08cff5ea56fa32367e5482e6a66875e109
Axel 豪豪 <BBRenHao35@gmail.com>
2023-08-31 00:51:23 +0800

    deploy

---
+++ assets/post_markdown.md.1b37bd59.js
@@ -0,0 +1,63 @@
+import{_ as s,o as a,c as n,Q as e}from"./chunks/framework.419948d5.js";const b=JSON.parse('{"title":"Markdown 語法","description":"","frontmatter":{},"headers":[],"relativePath":"post/markdown.md","filePath":"post/markdown.md","lastUpdated":1693414225000}'),l={name:"post/markdown.md"},p=e(`<h1 id="markdown-語法" tabindex="-1">Markdown 語法 <a class="header-anchor" href="#markdown-語法" aria-label="Permalink to &quot;Markdown 語法&quot;">​</a></h1><ul><li>紀錄比較少用、特殊的語法</li></ul><h2 id="表格雙行-小icon-強調文字" tabindex="-1">📌 表格雙行 + 小icon + 強調文字 <a class="header-anchor" href="#表格雙行-小icon-強調文字" aria-label="Permalink to &quot;:pushpin: 表格雙行 + 小icon + 強調文字&quot;">​</a></h2><table><thead><tr><th>⚠️ Warning</th></tr></thead><tbody><tr><td><code>Warning</code> Warning you will enter !!!</td></tr></tbody></table><table><thead><tr><th>❌ Error</th></tr></thead><tbody><tr><td><code>Error</code> This is the error message.</td></tr></tbody></table><table><thead><tr><th>ℹ️ Information</th></tr></thead><tbody><tr><td><code>Information</code> Detailed information content.</td></tr></tbody></table><table><thead><tr><th>✅ Success</th></tr></thead><tbody><tr><td><code>Success</code> Information returned successfully.</td></tr></tbody></table><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">| ⚠️ Warning                            | </span></span>
+<span class="line"><span style="color:#e1e4e8;">|----------------------------------------|</span></span>
+<span class="line"><span style="color:#e1e4e8;">|  \`Warning\` Warning you will enter !!!  |</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">| ❌ Error                          | </span></span>
+<span class="line"><span style="color:#e1e4e8;">|------------------------------------|</span></span>
+<span class="line"><span style="color:#e1e4e8;">| \`Error\` This is the error message. |</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">| ℹ️ Information                             | </span></span>
+<span class="line"><span style="color:#e1e4e8;">|---------------------------------------------|</span></span>
+<span class="line"><span style="color:#e1e4e8;">| \`Information\` Detailed information content. |</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">| ✅ Success                                  | </span></span>
+<span class="line"><span style="color:#e1e4e8;">|----------------------------------------------|</span></span>
+<span class="line"><span style="color:#e1e4e8;">| \`Success\` Information returned successfully. |</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">| ⚠️ Warning                            | </span></span>
+<span class="line"><span style="color:#24292e;">|----------------------------------------|</span></span>
+<span class="line"><span style="color:#24292e;">|  \`Warning\` Warning you will enter !!!  |</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">| ❌ Error                          | </span></span>
+<span class="line"><span style="color:#24292e;">|------------------------------------|</span></span>
+<span class="line"><span style="color:#24292e;">| \`Error\` This is the error message. |</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">| ℹ️ Information                             | </span></span>
+<span class="line"><span style="color:#24292e;">|---------------------------------------------|</span></span>
+<span class="line"><span style="color:#24292e;">| \`Information\` Detailed information content. |</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">| ✅ Success                                  | </span></span>
+<span class="line"><span style="color:#24292e;">|----------------------------------------------|</span></span>
+<span class="line"><span style="color:#24292e;">| \`Success\` Information returned successfully. |</span></span></code></pre></div><br><h2 id="icon" tabindex="-1">📌 Icon <a class="header-anchor" href="#icon" aria-label="Permalink to &quot;:pushpin: Icon&quot;">​</a></h2><blockquote><p><a href="https://github.com/markdown-it/markdown-it-emoji/blob/master/lib/data/full.json" target="_blank" rel="noreferrer">https://github.com/markdown-it/markdown-it-emoji/blob/master/lib/data/full.json</a></p></blockquote><p>👻 💯 ✋ ✂️ 👉 🔵 📌 👏 👶 🟪 🌿 👦 🍧 🍰 🕕</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">:ghost: :100: :hand: :scissors: :point_right: :large_blue_circle: :pushpin: </span></span>
+<span class="line"><span style="color:#e1e4e8;">:clap: :baby: :purple_square: :herb: :boy:  :shaved_ice: :cake: :clock6:</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">:ghost: :100: :hand: :scissors: :point_right: :large_blue_circle: :pushpin: </span></span>
+<span class="line"><span style="color:#24292e;">:clap: :baby: :purple_square: :herb: :boy:  :shaved_ice: :cake: :clock6:</span></span></code></pre></div><br><h2 id="區塊" tabindex="-1">📌 區塊 <a class="header-anchor" href="#區塊" aria-label="Permalink to &quot;:pushpin: 區塊&quot;">​</a></h2><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>Markdown Content <code>TIP</code></p></div><div class="info custom-block"><p class="custom-block-title">INFO</p><p>Markdown Content <code>info</code></p></div><div class="danger custom-block"><p class="custom-block-title">DANGER</p><p>Markdown Content <code>danger</code></p></div><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">:::tip</span></span>
+<span class="line"><span style="color:#e1e4e8;">Markdown Content \`TIP\`</span></span>
+<span class="line"><span style="color:#e1e4e8;">:::</span></span>
+<span class="line"><span style="color:#e1e4e8;">:::info</span></span>
+<span class="line"><span style="color:#e1e4e8;">Markdown Content \`info\`</span></span>
+<span class="line"><span style="color:#e1e4e8;">:::</span></span>
+<span class="line"><span style="color:#e1e4e8;">:::danger</span></span>
+<span class="line"><span style="color:#e1e4e8;">Markdown Content \`danger\`</span></span>
+<span class="line"><span style="color:#e1e4e8;">:::</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">:::tip</span></span>
+<span class="line"><span style="color:#24292e;">Markdown Content \`TIP\`</span></span>
+<span class="line"><span style="color:#24292e;">:::</span></span>
+<span class="line"><span style="color:#24292e;">:::info</span></span>
+<span class="line"><span style="color:#24292e;">Markdown Content \`info\`</span></span>
+<span class="line"><span style="color:#24292e;">:::</span></span>
+<span class="line"><span style="color:#24292e;">:::danger</span></span>
+<span class="line"><span style="color:#24292e;">Markdown Content \`danger\`</span></span>
+<span class="line"><span style="color:#24292e;">:::</span></span></code></pre></div><h2 id="表格" tabindex="-1">📌 表格 <a class="header-anchor" href="#表格" aria-label="Permalink to &quot;:pushpin: 表格&quot;">​</a></h2><table><thead><tr><th>Name</th><th>Color</th><th>price</th></tr></thead><tbody><tr><td>apple</td><td>Red</td><td>$20</td></tr><tr><td>banana</td><td>yellow</td><td>$10</td></tr><tr><td>lemon</td><td>green</td><td>$30</td></tr></tbody></table><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">| Name | Color | price | </span></span>
+<span class="line"><span style="color:#e1e4e8;">| ----- | ---- | ----- |</span></span>
+<span class="line"><span style="color:#e1e4e8;">| apple | Red | $20 |</span></span>
+<span class="line"><span style="color:#e1e4e8;">| banana | yellow | $10 |</span></span>
+<span class="line"><span style="color:#e1e4e8;">| lemon | green | $30 |</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">| Name | Color | price | </span></span>
+<span class="line"><span style="color:#24292e;">| ----- | ---- | ----- |</span></span>
+<span class="line"><span style="color:#24292e;">| apple | Red | $20 |</span></span>
+<span class="line"><span style="color:#24292e;">| banana | yellow | $10 |</span></span>
+<span class="line"><span style="color:#24292e;">| lemon | green | $30 |</span></span></code></pre></div><h2 id="粗體、底線、標記" tabindex="-1">📌 粗體、底線、標記 <a class="header-anchor" href="#粗體、底線、標記" aria-label="Permalink to &quot;:pushpin: 粗體、底線、標記&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">**粗體**</span></span>
+<span class="line"><span style="color:#e1e4e8;">++底線++</span></span>
+<span class="line"><span style="color:#e1e4e8;">==標記==</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">**粗體**</span></span>
+<span class="line"><span style="color:#24292e;">++底線++</span></span>
+<span class="line"><span style="color:#24292e;">==標記==</span></span></code></pre></div><h2 id="縮排" tabindex="-1">📌 縮排 <a class="header-anchor" href="#縮排" aria-label="Permalink to &quot;:pushpin: 縮排&quot;">​</a></h2><ul><li>清單一</li><li>清單二 <ul><li>清單二子項目</li></ul></li></ul><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">- 標號一</span></span>
+<span class="line"><span style="color:#e1e4e8;">- 標號二</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - 標號二子項目</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">- 標號一</span></span>
+<span class="line"><span style="color:#24292e;">- 標號二</span></span>
+<span class="line"><span style="color:#24292e;">    - 標號二子項目</span></span></code></pre></div><h2 id="分隔線" tabindex="-1">📌 分隔線 <a class="header-anchor" href="#分隔線" aria-label="Permalink to &quot;:pushpin: 分隔線&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">---</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">---</span></span></code></pre></div>`,29),o=[p];function t(c,r,i,d,h,y){return a(),n("div",null,o)}const g=s(l,[["render",t]]);export{b as __pageData,g as default};
